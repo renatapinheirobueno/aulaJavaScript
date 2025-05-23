5ee3c5cd1a9d41a5882f2cc603be19311d09024d
Renata Pinheiro <renata.pinheiro.bueno@escola.pr.gov.br>
2025-05-23 09:04:06 -0300

    Finalizando as atividades

---
+++ atividade1 copy.js
@@ -0,0 +1,9 @@
+function alternarVisibilidade(){
+    let caixa = document.getElementById("caixa");
+    if(caixa.style.display=="none"){
+        caixa.style.display="block";
+
+    } else{
+        caixa.style.display = "none"
+    }
+}
